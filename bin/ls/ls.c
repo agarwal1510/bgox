2c87a9522c47647299186ad15a468990aee7920c
bhaveshgoyal <bhavesh.goyal093@gmail.com>
2017-09-10 16:08:53 -0400

    Cleanup for wp1

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
+#define COMM_LEN 100
 int main(int argc, char *argv[], char *envp[]){
-	puts("lulu\n");
-	return 0;
+
+char buffer[COMM_LEN];
+int line = 0;
+int i = 0;
+while (read(0, &buffer[i], 1) > 0) {
+    if (buffer[i] == '\n') {
+        buffer[i] = 0;
+        line++;
+        i = 0;
+		puts(buffer);
+        continue;
+    }
+    i++;
+}
+return 0;
 }
--- crt/crt1.c
+++ crt/crt1.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
 void _start(void) {
-	main(0, NULL, NULL);
-	exit(0);
+/*char hello[10];
+int retval;
+__asm__ volatile ("int $0x80"
+ : "=a" (retval)
+  : "a" (3), "b" (1), "c" (hello), "d" (1)
+   : "memory");
+
+__asm__ volatile ("int $0x80"
+ : "=a" (retval)
+  : "a" (4), "b" (1), "c" (hello), "d" (sizeof(hello)-1)
+   : "memory");
+   __asm__ volatile ("int $0x80" : : "a" (1), "b" (0));
+*///	int ret =  
+	int ret = main(0, NULL, NULL);
+	exit(ret);
   // call main() and exit() here
 }
