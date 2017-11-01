500468f9620dc8482ae0ba53ca866c585f7e35e2
Aman <agarwal1510@gmail.com>
2017-11-01 14:01:15 -0400

    pdp and pml4 helper functions added

--- include/sys/ptops.h
+++ include/sys/ptops.h
@@ -16,6 +16,8 @@ bool pte_entry_is_writable(pte_entry e);
 
 bool pte_entry_is_present(pte_entry e);
 
+bool pte_entry_is_user(pte_entry e);
+
 void pde_entry_add_attr(pde_entry *e, uint64_t attr);
 
 void pde_entry_del_attr(pte_entry *e, uint64_t attr);
@@ -29,5 +31,33 @@ bool pde_entry_is_writable(pde_entry e);
 bool pde_entry_is_present(pde_entry e);
 
 bool pde_entry_is_user(pde_entry e);
+
+void pdp_entry_add_attr(pdp_entry *e, uint64_t attr);
+
+void pdp_entry_del_attr(ptp_entry *e, uint64_t attr);
+
+void pdp_entry_set_frame(pdp_entry *e, uint64_t phy_addr);
+
+uint64_t pdp_entry_get_pfn(pdp_entry e);
+
+bool pdp_entry_is_writable(pdp_entry e);
+
+bool pdp_entry_is_present(pdp_entry e);
+
+bool pdp_entry_is_user(pdp_entry e);
+
+void pml4_entry_add_attr(pml4_entry *e, uint64_t attr);
+
+void pml4_entry_del_attr(pml4_entry *e, uint64_t attr);
+
+void pml4_entry_set_frame(pml4_entry *e, uint64_t phy_addr);
+
+uint64_t pml4_entry_get_pfn(pml4_entry e);
+
+bool pml4_entry_is_writable(pml4_entry e);
+
+bool pml4_entry_is_present(pml4_entry e);
+
+bool pml4_entry_is_user(pml4_entry e);
 
 #endif
--- sys/ptops.c
+++ sys/ptops.c
@@ -35,12 +35,18 @@ bool pte_entry_is_present(pte_entry e){
 
 }
 
+bool pte_entry_is_user(pte_entry e){
+
+	return e & PTE_USER; // Return bits 12 - 48 for Frame Address
+
+}
+
 void pde_entry_add_attr(pde_entry *e, uint64_t attr){
 	
 	*e |= (attr);
 }
 
-void pde_entry_del_attr(pte_entry *e, uint64_t attr){
+void pde_entry_del_attr(pde_entry *e, uint64_t attr){
 	
 	*e &= ~(attr);
 }
@@ -72,5 +78,85 @@ bool pde_entry_is_present(pde_entry e){
 bool pde_entry_is_user(pde_entry e){
 
 	return e & PDE_USER; // Return bits 12 - 48 for Frame Address
+
+}
+
+void pdp_entry_add_attr(pdp_entry *e, uint64_t attr){
+
+        *e |= (attr);
+}
+
+void pdp_entry_del_attr(pdp_entry *e, uint64_t attr){
+
+        *e &= ~(attr);
+}
+
+void pdp_entry_set_frame(pdp_entry *e, uint64_t phy_addr){
+
+        *e = (*e & 0xffful) | phy_addr;
+
+}
+
+uint64_t pdp_entry_get_pfn(pdp_entry e){
+
+        return e & PDP_FRAME; // Return bits 12 - 48 for Frame Address
+
+}
+
+bool pdp_entry_is_writable(pdp_entry e){
+
+        return e & PDP_WRITABLE; // Return bits 12 - 48 for Frame Address
+
+}
+
+bool pdp_entry_is_present(pdp_entry e){
+
+        return e & PDP_PRESENT; // Return bits 12 - 48 for Frame Address
+
+}
+
+bool pdp_entry_is_user(pdp_entry e){
+
+        return e & PDP_USER; // Return bits 12 - 48 for Frame Address
+
+}
+
+void pml4_entry_add_attr(pml4_entry *e, uint64_t attr){
+
+        *e |= (attr);
+}
+
+void pml4_entry_del_attr(pml4_entry *e, uint64_t attr){
+
+        *e &= ~(attr);
+}
+
+void pml4_entry_set_frame(pml4_entry *e, uint64_t phy_addr){
+
+        *e = (*e & 0xffful) | phy_addr;
+
+}
+
+uint64_t pml4_entry_get_pfn(pml4_entry e){
+
+        return e & PML4_FRAME; // Return bits 12 - 48 for Frame Address
+
+}
+
+bool pml4_entry_is_writable(pml4_entry e){
+
+        return e & PML4_WRITABLE; // Return bits 12 - 48 for Frame Address
+
+}
+
+bool pml4_entry_is_present(pml4_entry e){
+
+        return e & PML4_PRESENT; // Return bits 12 - 48 for Frame Address
+
+}
+
+bool pml4_entry_is_user(pml4_entry e){
+
+        return e & PML4_USER; // Return bits 12 - 48 for Frame Address
 
 }
